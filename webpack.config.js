ad6509d901fb284c775d6217ed72ed6753997c8b
hwalborn <hwalborn@gmail.com>
2017-08-04 13:31:06 -0400

    added react and webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+const APP_DIR           = path.resolve(__dirname, 'src');
+const BUILD_DIR         = path.resolve(__dirname, 'dist');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+  entry: `${APP_DIR}/index.js`,
+  output: {
+    path: BUILD_DIR,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css/,
+        include: path.resolve(__dirname, "./app"),
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)/,
+        include: path.resolve(__dirname, "./app"),
+        loader: "url-loader"
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: [ "react", "es2015" ]
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ ".js" ]
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 8000
+  }
+}
